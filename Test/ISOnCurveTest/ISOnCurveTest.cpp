ad06dbdcfe701c489bc6514758eae2c8e2d14d3e
zhr2001 <2641489927@qq.com>
2023-02-04 03:17:21 +0800

    is on curve function

---
+++ Test/ISOnCurveTest/ISOnCurveTest.cpp
@@ -0,0 +1,9 @@
+#include "../../include/ECDSA.h"
+
+int main() {
+    point *BP = createPoint("79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798", 
+    "483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8");
+    EC *group = createEC("fffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f", 
+    "fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141", BP, 0, 7, 1);
+    if (is_on_curve(BP, group)) printf("Pass!\n");
+}
