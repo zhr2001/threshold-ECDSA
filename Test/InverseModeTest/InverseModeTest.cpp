0767208fa24cee7b2d88e604a91a35fab603eea2
zhr2001 <2641489927@qq.com>
2023-02-04 02:30:25 +0800

    Fix Inverse Mode

--- Test/InverseModeTest/InverseModeTest.cpp
+++ Test/InverseModeTest/InverseModeTest.cpp
@@ -2,9 +2,12 @@
 
 int main() {
     mpz_t k, p, correctRes;
-    mpz_init_set_str(k, "3", 10);
-    mpz_init_set_str(p, "5", 10);
+    mpz_init_set_str(k, "7", 10);
+    mpz_init_set_str(p, "11", 10);
+    gmp_printf("k= %Zd\n",k);
+    gmp_printf("p= %Zd\n",p);
     mpz_t *res = inverse_mod(k, p);
-    mpz_init_set_str(correctRes, "2", 10);
+    mpz_init_set_str(correctRes, "8", 10);
+    gmp_printf("correct= %Zd\n",correctRes);
     gmp_printf("res= %Zd\n",*res);
 }
