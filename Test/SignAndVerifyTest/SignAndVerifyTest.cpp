4f5d906710dda882b1d7a7a2670ecef4e2c8fff9
zhr2001 <2641489927@qq.com>
2023-02-04 16:53:04 +0800

    Pass Verify

--- Test/SignAndVerifyTest/SignAndVerifyTest.cpp
+++ Test/SignAndVerifyTest/SignAndVerifyTest.cpp
@@ -11,18 +11,23 @@ int main() {
     "fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141", 
     BP, 0, 7, 1);
 
-    mpz_t privateKey, temp;
-    point *publicKey = createPoint(
-    "abd9791437093d377ca25ea974ddc099eafa3d97c7250d2ea32af6a1556f92a", 
-    "3fe60f6150b6d87ae8d64b78199b13f26977407c801f233288c97ddc4acca326");
-    mpz_init_set_str(privateKey, "9f4c9eb899bd86e0e83ecca659602a15b2edb648e2ae4ee4a256b17bb29a1a1e", 16);
-    mpz_init_set(temp, privateKey);
-    point *test = scalar_multi(temp, group->BasePoint, group);
-    assert(mpz_cmp(test->x, publicKey->x) == 0 && mpz_cmp(test->y, publicKey->y) == 0);
+
+    key_pair *kp = make_keypair(group);
+
+    // mpz_t privateKey, temp;
+    // point *publicKey = createPoint(
+    // "abd9791437093d377ca25ea974ddc099eafa3d97c7250d2ea32af6a1556f92a", 
+    // "3fe60f6150b6d87ae8d64b78199b13f26977407c801f233288c97ddc4acca326");
+    // mpz_init_set_str(privateKey, "9f4c9eb899bd86e0e83ecca659602a15b2edb648e2ae4ee4a256b17bb29a1a1e", 16);
+    // mpz_init_set(temp, privateKey);
+    // point *test = scalar_multi(temp, group->BasePoint, group);
+    // assert(mpz_cmp(test->x, publicKey->x) == 0 && mpz_cmp(test->y, publicKey->y) == 0);
 
     char *message = "Hello!";
-    sign *sign = sign_message(privateKey, message, group);
+    sign *sign = sign_message(kp->privateKey, message, group);
     gmp_printf("Sign: (%Zd, %Zd)\n", sign->r, sign->s);
 
-    if (verifySignature(publicKey, message, sign, group)) printf("PASS!\n");
+    if (verifySignature(kp->publicKey, message, sign, group)) printf("PASS!\n");
 }
+
+// Sign: (89578011305203426357494261505270433926638756165426589388698937233968118474419, 9056892743108765099858605370117207548700216242494120761139563802023329858811)
