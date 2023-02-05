3c5e0c51bf3041f3d239cd6f1be7b44f4f289455
zhr2001 <2641489927@qq.com>
2023-02-05 14:29:44 +0800

    small

--- Test/SSSTest/SSSTest.cpp
+++ Test/SSSTest/SSSTest.cpp
@@ -4,7 +4,7 @@ int main() {
     mpz_t secret, p;
     mpz_init_set_str(secret, "fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141", 16);
     mpz_init_set_str(p,      "fffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f", 16);
-    SS *ss = createSS(3, 11, secret, p);
+    SS *ss = createSS(5, 11, secret, p);
     int sub[5] = {2,4,5,6,7};
     mpz_t part[5];
     mpz_init_set(part[0], ss->keyPartitions[1]);
@@ -12,7 +12,7 @@ int main() {
     mpz_init_set(part[2], ss->keyPartitions[4]);
     mpz_init_set(part[3], ss->keyPartitions[5]);
     mpz_init_set(part[4], ss->keyPartitions[6]);
-    DecryptoInfo d = {sub, 3, part};
+    DecryptoInfo d = {sub, 5, part};
     mpz_t *decrypted = combiner(&d, p);
     gmp_printf("Secret: %Zd\n", *decrypted);
 }
